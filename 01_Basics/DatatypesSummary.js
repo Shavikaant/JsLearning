81045b40c43e3a1afa037c14ef2081f6e3ec0e78
Kapil Rawat <147183030+Shavikaant@users.noreply.github.com>
2024-08-24 16:31:52 +0000

    change in location

---
+++ 01_Basics/DatatypesSummary.js
@@ -0,0 +1,11 @@
+ // Primitive
+
+ // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ // refernce (Non primitive)
+
+ // Array, Objects, functions
+
+ const bignum = 1234567890123451232094123213n
+
+ console.log(typeof bignum)
