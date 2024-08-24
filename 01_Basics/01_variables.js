75cacf806ff162fba51fb10de560baa2b3b32c1d
Kapil Rawat <147183030+Shavikaant@users.noreply.github.com>
2024-08-24 11:34:51 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+
+let accountEmail = "kapil@WebGLShaderPrecisionFormat.com"
+var accountPassword = "1234"
+accountCity = 'bikaner'
+
+// accountId = 2 // not allowed
+
+accountEmail = "huhu@susu.com"
+accountPassword = "12345"
+accountCity = "gtasa"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountCity,accountState])
+
+//Notes
+
+/*
+perfer not to use var
+because of issue in block scope and functionl scope
+*/
---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)// we are using node JS, not browser
+
+// code readablity should be high
+console.log(3 + 3)
+console.log("Kapil")
+
+
+let name = "kapil"
+
+let age = 12
+
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null (stand alone value, its a type)
+// undefine => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // Output = undefined
+console.log(typeof null); // output = object
