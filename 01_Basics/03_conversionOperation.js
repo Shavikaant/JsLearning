b48a210fa667f7a029f2ab6cb28a2161ce07eb8e
Kapil Rawat <147183030+Shavikaant@users.noreply.github.com>
2024-08-24 16:01:59 +0000

    Operations done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,9 +21,27 @@
 // "" = false
 // "hitesh" = true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringnum = String(someNumber);
+// let stringnum = String(someNumber);
 
-console.log(typeof stringnum);
+// console.log(typeof stringnum);
 
+// ****************************Operations***********************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " kapil"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log(1+2+"2");
+
+console.log(+true);
