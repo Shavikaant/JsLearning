2b95c0eb3880fdfba2eb5a762496aa7c853ad305
Kapil Rawat <147183030+Shavikaant@users.noreply.github.com>
2024-08-27 11:01:03 +0000

    new files add and changes have done

--- 01_Basics/05_Date&Time.js
+++
@@ -1,2 +0,0 @@
-// Dates
-console.log("kapil")
---
+++ 01_Basics/06_Arrays.js
@@ -0,0 +1,43 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["shaktiman","pepsi"]
+
+// console.log(myArr[0])
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop(7)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr)
+
+// slice, splice
+
+// console.log('a ',myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1)
+// console.log("b ",myArr)
+
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2)
+// console.log('c ',myArr)
+
+// myn1 = myArr.slice(1,2)
+
+// console.log('main array ref ',myArr)
+// console.log('splice stored in box',myn1)
+// console.log(myArr)
+
+let myDate = new Date();
+
+console.log(myDate.toDateString())
