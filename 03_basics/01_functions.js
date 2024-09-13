b3b7d8ece48de4755726101ddd0bc05bbf4f7cbc
Kapil Rawat <147183030+Shavikaant@users.noreply.github.com>
2024-09-13 21:09:14 +0000

    done f

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,36 @@ function logInUserMessage(username){
     }
 }
 
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
+
+function calculateCartPrice(...num1){
+
+    return num1
+
+}
+
+// console.log(calculateCartPrice(200,400,500))
+
+const user = {
+    username: "kapil",
+    price: 199
+}
+
+function handelObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handelObject(user)
+
+// handelObject({
+//     username: "Gabbar",
+//     price: "thakur"
+// })
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
